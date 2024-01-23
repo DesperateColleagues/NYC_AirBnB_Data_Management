ab5233cf5334d9fd96c1a5548c54c319ffffadce
Giacomo Detomaso <57029496+GiacomoDetomaso@users.noreply.github.com>
2024-01-23 16:35:58 +0100

    Closed issue #8

--- DML/neighborhood.sql
+++ DML/neighborhood.sql
@@ -4,29 +4,19 @@ ALTER TABLE neighborhoods RENAME COLUMN ntaname TO name;
 ALTER TABLE neighborhoods RENAME COLUMN boroname TO borough;
 ALTER TABLE neighborhoods RENAME COLUMN geom TO perimeter;
 
-ALTER TABLE neighborhoods 
-	DROP COLUMN borocode, DROP COLUMN countyfips,
-	DROP COLUMN nta2020, DROP COLUMN ntaabbrev,
-	DROP COLUMN cdta2020, DROP COLUMN ntatype, 
-	DROP COLUMN shape_leng, DROP COLUMN shape_area, 
-	DROP COLUMN cdtaname;
+CREATE TABLE neighborhoods_new AS (
+	SELECT id, name, borough, perimeter 
+	FROM neighborhoods
+);
+
+DROP TABLE neighborhoods;
+
+ALTER TABLE neighborhoods_new RENAME TO neighborhoods;
 
 -- 2) Turn the invalid geometries to valid ones
 UPDATE neighborhoods 
 SET perimeter = ST_MakeValid(perimeter)
 WHERE St_IsValid(perimeter) = 'False';
-
--- 3) Change perimeter column type in its text form
--- TODO: establish if this procedure is really needed
-/*
-ALTER TABLE neighborhoods ADD COLUMN perimeter_text text;
-
-UPDATE neighborhoods
-SET perimeter_text = ST_AsText(perimeter);
-
-ALTER TABLE neighborhoods DROP COLUMN perimeter;
-ALTER TABLE neighborhoods RENAME COLUMN perimeter_text TO perimeter;
-*/
 
 -- 4) Replace the name of the borough with its code (it will be FK key)
 UPDATE neighborhoods
--- DML/parks.sql
+++ DML/parks.sql
@@ -24,18 +24,6 @@ UPDATE parks
 SET perimeter = St_MakeValid(perimeter)
 WHERE ST_IsValid(perimeter) = 'False';
 
---- 5) Change perimeter column type in its text form
--- TODO: establish if this procedure is really needed
-/*
-ALTER TABLE parks ADD COLUMN perimeter_text text;
-
-UPDATE parks
-SET perimeter_text = St_AsText(perimeter);
-
-ALTER TABLE parks DROP COLUMN perimeter;
-ALTER TABLE parks RENAME COLUMN perimeter_text TO perimeter;
-*/
-
 -- 5) Add a column who shows the borough in which the park lies
 ALTER TABLE parks ADD COLUMN borough CHAR(2);
 
@@ -45,17 +33,8 @@ WHERE ST_Intersects(n.perimeter, p.perimeter)
 GROUP BY p.id, n.borough
 ORDER BY n DESC;
 
-UPDATE parks p
-SET borough = n.borough
-FROM neighborhoods2 n 
-WHERE ST_Intersects(n.perimeter, p.perimeter) 
-
 SELECT * FROM parks WHERE borough IS NULL;
 SELECT * FROM parks WHERE id = 78;
-
-SELECT DISTINCT p.id, p.name, n.borough, n.name, ST_Intersection(n.perimeter, p.perimeter)
-FROM parks p, neighborhoods2 n
-WHERE ST_Intersects(n.perimeter, p.perimeter) 
 
 
 
