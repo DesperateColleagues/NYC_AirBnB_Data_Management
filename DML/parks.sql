4bf5b001675d499509f7187cf4fe260dacf37db4
Giacomo Detomaso <57029496+GiacomoDetomaso@users.noreply.github.com>
2024-01-23 17:53:51 +0100

    Closed issue #5
    Added an array column to the park table that cotains the code of the boroughs in which the park is extended
    
    Co-Authored-By: Gabriele <99121423+GabrieleDetomaso@users.noreply.github.com>

--- DML/parks.sql
+++ DML/parks.sql
@@ -5,7 +5,7 @@ ALTER TABLE parks RENAME COLUMN typecatego TO category;
 
 -- 2) Drop useless columns by creating a new table from parks
 CREATE TABLE parks_new AS 
-	SELECT gid, signname, typecatego, geom AS perimeter 
+	SELECT id, name, category, geom AS perimeter 
 	FROM parks;
 
 DROP TABLE parks;
@@ -14,28 +14,24 @@ ALTER TABLE parks_new RENAME TO parks;
 
 -- 3) Remove parks tuples that are not actual parks
 DELETE FROM parks 
-WHERE typecatego IN (
+WHERE category IN (
 	'Strip', 'Lot', 'Parkway', 'Cemetery',
 	'Waterfront Facility', 'Undeveloped', 'Mall'
 );
 						 
 -- 4) Turn the invalid geometries to valid ones
 UPDATE parks 
-SET perimeter = St_MakeValid(perimeter)
+SET perimeter = ST_MakeValid(perimeter)
 WHERE ST_IsValid(perimeter) = 'False';
 
 -- 5) Add a column who shows the borough in which the park lies
-ALTER TABLE parks ADD COLUMN borough CHAR(2);
+ALTER TABLE parks ADD COLUMN boroughs CHAR(2)[];
 
-SELECT p.id, n.borough, COUNT(*) n
-FROM parks p, neighborhoods2 n 
-WHERE ST_Intersects(n.perimeter, p.perimeter) 
-GROUP BY p.id, n.borough
-ORDER BY n DESC;
-
-SELECT * FROM parks WHERE borough IS NULL;
-SELECT * FROM parks WHERE id = 78;
-
-
-
-
+UPDATE parks p1
+SET boroughs = (
+	SELECT ARRAY(
+		SELECT n.borough::char(2)
+		FROM parks p2, neighborhoods n 
+		WHERE ST_Intersects(n.perimeter, p2.perimeter) AND p2.id = P1.id
+		GROUP BY p2.id, n.borough
+))
