0d07d73afc16253afcdb077c987f449f7ec8d25a
GabrieleDetomaso <99121423+GabrieleDetomaso@users.noreply.github.com>
2024-01-24 18:18:01 +0100

    Closed issue #10
    - Created "poi" and "poi_type" tables.
    
    - Updated type and domain codes with the corresponding description

--- DDL/DDL.sql
+++ DDL/DDL.sql
@@ -3,3 +3,52 @@ CREATE TABLE IF NOT EXISTS borough (
 	id CHAR(2) NOT NULL PRIMARY KEY,
 	name VARCHAR(15)
 );
+
+-- This tables will contain every POIs and POIs' types
+ALTER TABLE poi RENAME COLUMN gid TO id;
+ALTER TABLE poi RENAME COLUMN faci_dom TO domain;
+ALTER TABLE poi RENAME COLUMN geom TO coordinates;
+
+CREATE TABLE poi_type AS (SELECT DISTINCT facility_t::INTEGER FROM poi);
+ALTER TABLE poi_type RENAME COLUMN facility_t TO id;
+
+ALTER TABLE poi_type ADD COLUMN type_desc VARCHAR(50);
+
+CREATE TABLE POI_NEW AS (
+	SELECT id, name, domain, facility_t::INTEGER as poi_type, coordinates FROM poi
+);
+
+DROP TABLE poi;
+
+ALTER TABLE POI_NEW RENAME TO poi;
+
+-- This table will contain every neighborhoods
+ALTER TABLE neighborhoods RENAME COLUMN gid TO id;
+ALTER TABLE neighborhoods RENAME COLUMN ntaname TO name;
+ALTER TABLE neighborhoods RENAME COLUMN boroname TO borough;
+ALTER TABLE neighborhoods RENAME COLUMN geom TO perimeter;
+
+CREATE TABLE neighborhoods_new AS (
+	SELECT id, name, borough, perimeter 
+	FROM neighborhoods
+);
+
+DROP TABLE neighborhoods;
+
+ALTER TABLE neighborhoods_new RENAME TO neighborhoods;
+
+-- This table will contain every park
+ALTER TABLE parks RENAME COLUMN gid TO id;
+ALTER TABLE parks RENAME COLUMN signname TO name;
+ALTER TABLE parks RENAME COLUMN typecatego TO category;
+
+CREATE TABLE parks_new AS 
+	SELECT gid, signname, typecatego, geom AS perimeter 
+	FROM parks;
+
+DROP TABLE parks;
+
+ALTER TABLE parks_new RENAME TO parks; 
+
+-- Add a column who shows the borough in which the park lies
+ALTER TABLE parks ADD COLUMN boroughs CHAR(2)[];
--- DML/parks.sql
+++ DML/parks.sql
@@ -1,31 +1,14 @@
--- 1) Rename columns to match schema
-ALTER TABLE parks RENAME COLUMN gid TO id;
-ALTER TABLE parks RENAME COLUMN signname TO name;
-ALTER TABLE parks RENAME COLUMN typecatego TO category;
-
--- 2) Drop useless columns by creating a new table from parks
-CREATE TABLE parks_new AS 
-	SELECT id, name, category, geom AS perimeter 
-	FROM parks;
-
-DROP TABLE parks;
-
-ALTER TABLE parks_new RENAME TO parks; 
-
--- 3) Remove parks tuples that are not actual parks
+-- 1) Remove parks tuples that are not actual parks
 DELETE FROM parks 
 WHERE category IN (
 	'Strip', 'Lot', 'Parkway', 'Cemetery',
 	'Waterfront Facility', 'Undeveloped', 'Mall'
 );
 						 
--- 4) Turn the invalid geometries to valid ones
+-- 2) Turn the invalid geometries to valid ones
 UPDATE parks 
 SET perimeter = ST_MakeValid(perimeter)
 WHERE ST_IsValid(perimeter) = 'False';
-
--- 5) Add a column who shows the borough in which the park lies
-ALTER TABLE parks ADD COLUMN boroughs CHAR(2)[];
 
 UPDATE parks p1
 SET boroughs = (
